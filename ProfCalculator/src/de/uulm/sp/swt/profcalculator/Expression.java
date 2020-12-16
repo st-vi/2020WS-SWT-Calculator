b3dd5b033e4405984665b2bc290c37a4d431f47c
Thomas Thüm <thomas.thuem@uni-ulm.de>
2020-12-16 18:39:50 +0100

    refactor: Push down fields from class Expression

--- ProfCalculator/src/de/uulm/sp/swt/profcalculator/Expression.java
+++ ProfCalculator/src/de/uulm/sp/swt/profcalculator/Expression.java
@@ -2,9 +2,6 @@ package de.uulm.sp.swt.profcalculator;
 
 public abstract class Expression {
 
-	public Value left;
-	public Value right;
-
 	public abstract int evaluate();
 
 	public String computeEquation() {
